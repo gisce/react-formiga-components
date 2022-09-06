658f902017a96f90e3a77fa0085e649458fe6af6
Marc Güell Segarra <marc@ondori.dev>
2022-09-06 16:28:20 +0200

    Improve CharInput

--- src/components/widgets/Char/CharInput/CharInput.tsx
+++ src/components/widgets/Char/CharInput/CharInput.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Input } from "antd";
 import { CharInputProps } from "./CharInput.types";
 import { RequiredCharInput, RequiredPasswordInput } from "./CharInput.styles";
+import { ThemeConfig } from "@/theme";
 
 export const CharInput = (props: CharInputProps) => {
   const {
@@ -14,22 +15,17 @@ export const CharInput = (props: CharInputProps) => {
   const InputComponent = required && !readOnly ? RequiredCharInput : Input;
   const InputPasswordComponent =
     required && !readOnly ? RequiredPasswordInput : Input.Password;
+  const requiredClass =
+    required && !readOnly ? ThemeConfig.requiredClass : undefined;
+  const Component = isPassword ? InputPasswordComponent : InputComponent;
+  const compProps = {
+    disabled: readOnly,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(e.target.value);
+    },
+    value,
+    className: requiredClass,
+  };
 
-  return isPassword ? (
-    <InputPasswordComponent
-      disabled={readOnly}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
-      value={value}
-    />
-  ) : (
-    <InputComponent
-      disabled={readOnly}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
-      value={value}
-    />
-  );
+  return <Component {...compProps} />;
 };
