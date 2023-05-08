6197e2dd1d619325d50f639c2cdbe7797c5bbf9d
Marc Güell Segarra <marc@ondori.dev>
2023-05-08 10:42:53 +0200

    Add icon support for groups

--- src/components/ui/FieldSet/FieldSet.tsx
+++ src/components/ui/FieldSet/FieldSet.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { FieldSetElement, Legend } from "./FieldSet.styles";
 import { FieldSetProps } from "./FieldSet.types";
+import { Space } from "antd";
 
 export const FieldSet = (props: FieldSetProps): React.ReactElement => {
-  const { label, children } = props;
+  const { label, children, icon: Icon } = props;
+  const labelComponent = (
+    <Space>
+      {Icon ? <Icon /> : null}
+      {label}
+    </Space>
+  );
 
   return (
     <FieldSetElement>
-      <Legend>{label}</Legend>
+      <Legend>{labelComponent}</Legend>
       {children}
     </FieldSetElement>
   );
