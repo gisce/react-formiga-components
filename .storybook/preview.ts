523b7f7694f0593e99cb379359ef7dfe983ddcdc
Marc Güell Segarra <marc@ondori.dev>
2023-12-18 16:53:27 +0100

    fix: add storybook again

---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import "./tailwind.css";
+import { Buffer } from "buffer";
+
+window.global = window;
+window.Buffer = window.Buffer || Buffer;
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
