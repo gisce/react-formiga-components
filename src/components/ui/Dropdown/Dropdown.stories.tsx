779737bb16e703fab5839a97c7a404e47f84f38e
Marc Güell Segarra <marc@ondori.dev>
2024-01-15 14:30:29 +0100

    feat(New dropdown): new dropdown WIP

--- src/components/ui/Dropdown/Dropdown.stories.tsx
+++ src/components/ui/Dropdown/Dropdown.stories.tsx
@@ -1,21 +1,67 @@
 import { FC } from "react";
 import { Dropdown } from "./Dropdown";
+import { MobileOutlined } from "@ant-design/icons";
+import { DropdownMenuGroup } from "./Dropdown.types";
 
 export default {
   title: "Components/Dropdown",
   component: Dropdown,
 };
 
-const DropdownWrapper = ({}) => {
-  // Additional logic or state can be added here if needed
+const exampleData: DropdownMenuGroup[] = [
+  {
+    label: "Group 1",
+    items: [
+      { id: 1, name: "Item 1" },
+      { id: 2, name: "Item 2" },
+    ],
+  },
+  {
+    label: "Group 2",
+    items: [
+      { id: 3, name: "Item 3" },
+      { id: 4, name: "Item 4" },
+    ],
+  },
+  {
+    label: "Group 3",
+    items: [
+      { id: 5, name: "Item 5" },
+      { id: 6, name: "Item 6" },
+    ],
+  },
+  {
+    label: "Group 4",
+    items: [
+      { id: 7, name: "Item 7" },
+      { id: 8, name: "Item 8" },
+    ],
+  },
+  {
+    label: "Group 5",
+    items: [
+      { id: 9, name: "Item 9" },
+      { id: 10, name: "Item 10" },
+    ],
+  },
+];
 
-  const handleToggle = () => {
-    console.log("Dropdown toggled");
-  };
-
-  return <Dropdown />;
-};
-
-const Template: FC<any> = (args) => <DropdownWrapper {...args} />;
+const Template: FC<any> = (args) => <Dropdown {...args} />;
 
 export const Default = Template.bind({});
+Default.exampleData = exampleData;
+Default.args = {
+  data: exampleData,
+  children: (
+    <button
+      style={{
+        backgroundColor: "#cf2",
+        padding: 20,
+        borderRadius: 10,
+      }}
+    >
+      Hover me
+    </button>
+  ),
+  trigger: ["hover"],
+};
--- src/components/ui/Dropdown/index.ts
+++ src/components/ui/Dropdown/index.ts
@@ -1,1 +1,3 @@
 export * from "./Dropdown";
+export * from "./DropdownButton";
+export * from "./Dropdown.types";
