6a6519413b962c6da8d7bcaa2e56523494146f91
Marc Güell Segarra <marc@ondori.dev>
2024-01-19 11:23:44 +0100

    feat(New dropdown): add new onOpenChange prop in Dropdown and Many2oneSuffix

--- src/components/widgets/Many2One/Many2OneSuffix/Many2OneSuffix.types.tsx
+++ src/components/widgets/Many2One/Many2OneSuffix/Many2OneSuffix.types.tsx
@@ -3,4 +3,5 @@ import { DropdownMenuGroup, DropdownMenuItem } from "@/components/ui";
 export type Many2OneSuffixProps = {
   onRetrieveData: () => Promise<DropdownMenuGroup[]>;
   onItemClick?: (item: DropdownMenuItem) => void;
+  onOpenChange?: (open: boolean) => void;
 };
