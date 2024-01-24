076f6e552d90b3af626be7cd416ef137ab79df5e
mguellsegarra <mguellsegarra@users.noreply.github.com>
2024-01-24 08:50:02 +0000

    deploy: 376a8b03f51c4ae97bd435718783b423b045ccd9

---
+++ alpha/assets/ExportModal.stories-5dcb3a6d.js
@@ -0,0 +1,256 @@
+import{j as i}from"./jsx-runtime-f6d73c06.js";import{r as d}from"./index-8ee6c85d.js";import{E as l}from"./ExportModal-15fa02c3.js";import{B as c}from"./button-5dad7698.js";import"./_commonjsHelpers-de833af9.js";import"./ModalBottomBar-35f39ea1.js";import"./theme-ac0bd082.js";import"./LocaleContext-e44580cc.js";import"./row-a9151da8.js";import"./index-f36c3419.js";import"./assertThisInitialized-4fab7dd0.js";import"./index-8eb238e3.js";import"./index-298124e7.js";import"./toArray-d3285e7a.js";import"./Compact-f42963ce.js";import"./CloseOutlined-d6b1b11b.js";import"./index-d8bbe3ff.js";import"./isVisible-28761958.js";import"./compact-item-c03ebbb7.js";import"./CheckOutlined-9e94d9a6.js";import"./SaveOutlined-8f95811b.js";import"./Button-bb28d1e9.js";import"./ButtonWithTooltip-d2c6a913.js";import"./index-f3897118.js";import"./motion-343f592f.js";import"./roundedArrow-eeec1c6c.js";import"./reactNode-c432c2b0.js";import"./styled-components.browser.esm-f71162e2.js";import"./Label-517a773f.js";import"./Separator-0c8127be.js";import"./index-f7e7ddd9.js";import"./FieldSet-7cd4746b.js";import"./Dropdown-2ac96ff7.js";import"./FormigaConfig-4836e33b.js";import"./index-a232289b.js";import"./omit-c2a66ff4.js";import"./index-7f6f4650.js";import"./context-1cb86123.js";import"./asyncToGenerator-70b03fb5.js";import"./index-8a2d9a84.js";import"./useVariants-98bbef89.js";import"./CloseCircleFilled-8383c3aa.js";import"./SearchOutlined-84e5fa13.js";import"./CheckCircleFilled-a3a9ebd2.js";import"./ExclamationCircleFilled-72a8eb89.js";import"./InfoCircleFilled-0bb2125c.js";import"./PurePanel-7427106a.js";import"./RightOutlined-aee7cace.js";import"./Overflow-be9dddcb.js";import"./slide-cc98ec73.js";import"./collapse-97de76d4.js";import"./DropdownButton-f6cd65f6.js";import"./DownOutlined-67df1f86.js";import"./index-c6e1badc.js";import"./index-2c809c7b.js";import"./index-5c562ca6.js";import"./useLocale-cc382a13.js";import"./index-d971594d.js";import"./FolderOpenOutlined-33e43c68.js";import"./EditOutlined-73a4ba58.js";const ve={title:"Components/Export/ExportModal",component:l},n=()=>{const[p,a]=d.useState(!1);return i.jsxs(i.Fragment,{children:[i.jsx(c,{onClick:()=>{a(!0)},children:"Open modal"}),i.jsx(l,{visible:p,onCancel:()=>{a(!1)},onSucceed:async e=>{await new Promise(t=>setTimeout(t,1e3)),a(!1),console.log(e)},locale:"en_US",selectedKeys:[],totalRegisters:92348,visibleRegisters:8,selectedRegistersToExport:32,onGetFieldChilds:async e=>(console.log("onGetFieldChilds: "+e),await new Promise(t=>setTimeout(t,1e3)),e==="active"?[{key:"active/contador",title:"Activo/Contador",tooltip:"Help",isLeaf:!0},{key:"active/code",title:"Activo/Código",isLeaf:!0,required:!0}]:e==="parent1"?[{key:"parent1/parent2",title:"parent1/parent2",tooltip:"Help",isLeaf:!1}]:e==="parent1/parent2"?[{key:"parent1/parent2/test",title:"parent1/parent2/test",tooltip:"Help",isLeaf:!0}]:[]),onGetFields:async()=>(await new Promise(e=>setTimeout(e,1e3)),[{key:"active",title:"Activo",isLeaf:!1},{key:"parent1",title:"Parent1",isLeaf:!1},{key:"address/id",title:"Contactos",isLeaf:!1},{key:"bank_ids/id",title:"Bancos",isLeaf:!1},{key:"category_id/id",title:"Categorías",isLeaf:!1},{key:"child_ids/id",title:"Ref. empresa",isLeaf:!1},{key:"cifnif",title:"CIF/NIF",isLeaf:!0},{key:"city",title:"Ciudad",isLeaf:!0},{key:"codigo_contable",title:"Código contable",isLeaf:!0},{key:"comercial",title:"Nombre comercial",isLeaf:!0},{key:"comment",title:"Notas",isLeaf:!0},{key:"credit",title:"Total a cobrar",tooltip:"Importe total que este cliente le debe.",isLeaf:!0},{key:"credit_limit",title:"Crédito concedido",isLeaf:!0},{key:"customer",title:"Cliente",tooltip:"Marque esta opción si la empresa es un cliente.",isLeaf:!0},{key:"date",title:"Fecha",isLeaf:!0},{key:"debit",title:"Total a pagar",tooltip:"Importe total que debe pagar a este proveedor.",isLeaf:!0},{key:"debit_limit",title:"Límite a pagar",isLeaf:!0},{key:"ean13",title:"EAN13",isLeaf:!0},{key:"emails",title:"Emails",isLeaf:!0},{key:"energy_sector",title:"Sector de energía",isLeaf:!0},{key:"events/id",title:"Eventos",isLeaf:!1},{key:"has_debt",title:"Tiene deuda",isLeaf:!0},{key:"lang",title:"Idioma",tooltip:"Si el idioma seleccionado está instalado en el sistema, todos los documentos relacionados con esta empresa serán mostrados en este idioma. Si no, serán mostrados en inglés.",isLeaf:!0},{key:"name",title:"Nombre",required:!0,isLeaf:!0},{key:"ov_users_ids/id",title:"Usuarios Oficina Virtual",isLeaf:!1},{key:"parent_id/id",title:"Empresa principal",isLeaf:!1},{key:"payment_type_customer/id",title:"Tipo de pago de cliente",tooltip:"Tipo de pago como cliente.",isLeaf:!1},{key:"payment_type_supplier/id",title:"Tipo de pago de proveedor",tooltip:"Tipo de pago como proveedor.",isLeaf:!1},{key:"ref",title:"Código",isLeaf:!0},{key:"ref2",title:"Ref2",isLeaf:!0},{key:"ref_companies/id",title:"Compañías que se refieren a la empresa",isLeaf:!1},{key:"representante_id/id",title:"Representante",isLeaf:!1},{key:"supplier",title:"Proveedor",tooltip:"Marque esta opción si la empresa es un proveedor. Si no está marcada no será visible al introducir un pedido de compra.",isLeaf:!0},{key:"title",title:"Título",isLeaf:!0},{key:"total_debt",title:"Deuda Total",tooltip:"Cantidad total que este cliente le debe.",isLeaf:!0},{key:"user_id/id",title:"Comercial dedicado",tooltip:"\uFEFFEl usuario interno que se encarga de comunicarse con esta empresa si hay.",isLeaf:!1},{key:"vat",title:"CIF/NIF",tooltip:"Número CIF/NIF. Marque esta caja si la empresa está sujeta al IVA. Se utiliza para la declaración legal del IVA.",isLeaf:!0},{key:"vat_subjected",title:"Declaración Legal del NIF",tooltip:"Marque esta casilla si el partner está sujeto al NIF. Se utilizará para la declaración legal del NIF.",isLeaf:!0},{key:"website",title:"Sitio web",isLeaf:!0}]),onGetPredefinedExports:async()=>(await new Promise(e=>setTimeout(e,1e3)),[{id:3,name:"Exportación de prueba 3",fields:[{key:"active/contador"},{key:"parent1/parent2/test"}]},{id:1,name:"Exportación de prueba",fields:[{key:"active"}]},{id:2,name:"Exportación de prueba 2",fields:[{key:"city"}]}]),onSavePredefinedExport:async e=>(await new Promise(t=>setTimeout(t,3e3)),console.log("onSavePredefinedExport: "+JSON.stringify(e,null,2)),{...e,id:e.id?e.id:Math.floor(Math.random()*1e3)}),onRemovePredefinedExport:async e=>(console.log("onRemovePredefinedExport: "+JSON.stringify(e,null,2)),await new Promise(t=>setTimeout(t,3e3)))})]})};var o,r,s;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
+  const [visible, setVisible] = useState(false);
+  return <>
+      <Button onClick={() => {
+      setVisible(true);
+    }}>
+        Open modal
+      </Button>
+      <ExportModal visible={visible} onCancel={() => {
+      setVisible(false);
+    }} onSucceed={async (options: ExportOptions) => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      // throw new Error("Example error");
+      setVisible(false);
+      console.log(options);
+      return;
+    }} locale="en_US"
+    // selectedKeys={["active"]}
+    selectedKeys={[]} totalRegisters={92348} visibleRegisters={8} selectedRegistersToExport={32} onGetFieldChilds={async (key: string) => {
+      console.log("onGetFieldChilds: " + key);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      if (key === "active") {
+        return [{
+          key: "active/contador",
+          title: "Activo/Contador",
+          tooltip: "Help",
+          isLeaf: true
+        }, {
+          key: "active/code",
+          title: "Activo/Código",
+          isLeaf: true,
+          required: true
+        }];
+      } else if (key === "parent1") {
+        return [{
+          key: "parent1/parent2",
+          title: "parent1/parent2",
+          tooltip: "Help",
+          isLeaf: false
+        }];
+      } else if (key === "parent1/parent2") {
+        return [{
+          key: "parent1/parent2/test",
+          title: "parent1/parent2/test",
+          tooltip: "Help",
+          isLeaf: true
+        }];
+      }
+      return [];
+    }} onGetFields={async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      return [{
+        key: "active",
+        title: "Activo",
+        isLeaf: false
+      }, {
+        key: "parent1",
+        title: "Parent1",
+        isLeaf: false
+      }, {
+        key: "address/id",
+        title: "Contactos",
+        isLeaf: false
+      }, {
+        key: "bank_ids/id",
+        title: "Bancos",
+        isLeaf: false
+      }, {
+        key: "category_id/id",
+        title: "Categorías",
+        isLeaf: false
+      }, {
+        key: "child_ids/id",
+        title: "Ref. empresa",
+        isLeaf: false
+      }, {
+        key: "cifnif",
+        title: "CIF/NIF",
+        isLeaf: true
+      }, {
+        key: "city",
+        title: "Ciudad",
+        isLeaf: true
+      }, {
+        key: "codigo_contable",
+        title: "Código contable",
+        isLeaf: true
+      }, {
+        key: "comercial",
+        title: "Nombre comercial",
+        isLeaf: true
+      }, {
+        key: "comment",
+        title: "Notas",
+        isLeaf: true
+      }, {
+        key: "credit",
+        title: "Total a cobrar",
+        tooltip: "Importe total que este cliente le debe.",
+        isLeaf: true
+      }, {
+        key: "credit_limit",
+        title: "Crédito concedido",
+        isLeaf: true
+      }, {
+        key: "customer",
+        title: "Cliente",
+        tooltip: "Marque esta opción si la empresa es un cliente.",
+        isLeaf: true
+      }, {
+        key: "date",
+        title: "Fecha",
+        isLeaf: true
+      }, {
+        key: "debit",
+        title: "Total a pagar",
+        tooltip: "Importe total que debe pagar a este proveedor.",
+        isLeaf: true
+      }, {
+        key: "debit_limit",
+        title: "Límite a pagar",
+        isLeaf: true
+      }, {
+        key: "ean13",
+        title: "EAN13",
+        isLeaf: true
+      }, {
+        key: "emails",
+        title: "Emails",
+        isLeaf: true
+      }, {
+        key: "energy_sector",
+        title: "Sector de energía",
+        isLeaf: true
+      }, {
+        key: "events/id",
+        title: "Eventos",
+        isLeaf: false
+      }, {
+        key: "has_debt",
+        title: "Tiene deuda",
+        isLeaf: true
+      }, {
+        key: "lang",
+        title: "Idioma",
+        tooltip: "Si el idioma seleccionado está instalado en el sistema, todos los documentos relacionados con esta empresa serán mostrados en este idioma. Si no, serán mostrados en inglés.",
+        isLeaf: true
+      }, {
+        key: "name",
+        title: "Nombre",
+        required: true,
+        isLeaf: true
+      }, {
+        key: "ov_users_ids/id",
+        title: "Usuarios Oficina Virtual",
+        isLeaf: false
+      }, {
+        key: "parent_id/id",
+        title: "Empresa principal",
+        isLeaf: false
+      }, {
+        key: "payment_type_customer/id",
+        title: "Tipo de pago de cliente",
+        tooltip: "Tipo de pago como cliente.",
+        isLeaf: false
+      }, {
+        key: "payment_type_supplier/id",
+        title: "Tipo de pago de proveedor",
+        tooltip: "Tipo de pago como proveedor.",
+        isLeaf: false
+      }, {
+        key: "ref",
+        title: "Código",
+        isLeaf: true
+      }, {
+        key: "ref2",
+        title: "Ref2",
+        isLeaf: true
+      }, {
+        key: "ref_companies/id",
+        title: "Compañías que se refieren a la empresa",
+        isLeaf: false
+      }, {
+        key: "representante_id/id",
+        title: "Representante",
+        isLeaf: false
+      }, {
+        key: "supplier",
+        title: "Proveedor",
+        tooltip: "Marque esta opción si la empresa es un proveedor. Si no está marcada no será visible al introducir un pedido de compra.",
+        isLeaf: true
+      }, {
+        key: "title",
+        title: "Título",
+        isLeaf: true
+      }, {
+        key: "total_debt",
+        title: "Deuda Total",
+        tooltip: "Cantidad total que este cliente le debe.",
+        isLeaf: true
+      }, {
+        key: "user_id/id",
+        title: "Comercial dedicado",
+        tooltip: "\uFEFFEl usuario interno que se encarga de comunicarse con esta empresa si hay.",
+        isLeaf: false
+      }, {
+        key: "vat",
+        title: "CIF/NIF",
+        tooltip: "Número CIF/NIF. Marque esta caja si la empresa está sujeta al IVA. Se utiliza para la declaración legal del IVA.",
+        isLeaf: true
+      }, {
+        key: "vat_subjected",
+        title: "Declaración Legal del NIF",
+        tooltip: "Marque esta casilla si el partner está sujeto al NIF. Se utilizará para la declaración legal del NIF.",
+        isLeaf: true
+      }, {
+        key: "website",
+        title: "Sitio web",
+        isLeaf: true
+      }];
+    }} onGetPredefinedExports={async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      return [{
+        id: 3,
+        name: "Exportación de prueba 3",
+        fields: [{
+          key: "active/contador"
+        }, {
+          key: "parent1/parent2/test"
+        }]
+      }, {
+        id: 1,
+        name: "Exportación de prueba",
+        fields: [{
+          key: "active"
+        }]
+      }, {
+        id: 2,
+        name: "Exportación de prueba 2",
+        fields: [{
+          key: "city"
+        }]
+      }];
+    }} onSavePredefinedExport={async (options: PredefinedExport) => {
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      console.log("onSavePredefinedExport: " + JSON.stringify(options, null, 2));
+      return {
+        ...options,
+        id: options.id ? options.id : Math.floor(Math.random() * 1000)
+      };
+    }} onRemovePredefinedExport={async (options: PredefinedExport) => {
+      console.log("onRemovePredefinedExport: " + JSON.stringify(options, null, 2));
+      return await new Promise(resolve => setTimeout(resolve, 3000));
+    }} />
+    </>;
+}`,...(s=(r=n.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const ge=["Default"];export{n as Default,ge as __namedExportsOrder,ve as default};
