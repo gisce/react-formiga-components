f0f53230e31ebd67a7dab100eca81ea89b3366bd
mguellsegarra <mguellsegarra@users.noreply.github.com>
2024-02-01 07:43:35 +0000

    deploy: 95be51ad3876daa1d82a964fd16008627c43efc0

---
+++ develop/assets/BooleanSearch.stories-f65f2ec3.js
@@ -0,0 +1,10 @@
+import{j as l}from"./jsx-runtime-f6d73c06.js";import{r as p}from"./index-8ee6c85d.js";import{B as n}from"./BooleanSearch-2332781c.js";import{L as c}from"./BaseField.argTypes-d72d00ba.js";import"./_commonjsHelpers-de833af9.js";import"./SelectionInput-222e5ca1.js";import"./styled-components.browser.esm-f71162e2.js";import"./index-f36c3419.js";import"./assertThisInitialized-4fab7dd0.js";import"./index-8eb238e3.js";import"./index-d971594d.js";import"./isVisible-28761958.js";import"./index-d8bbe3ff.js";import"./toArray-d3285e7a.js";import"./PurePanel-7427106a.js";import"./Overflow-be9dddcb.js";import"./omit-c2a66ff4.js";import"./motion-343f592f.js";import"./useVariants-98bbef89.js";import"./context-1cb86123.js";import"./asyncToGenerator-70b03fb5.js";import"./useLocale-cc382a13.js";import"./Compact-f42963ce.js";import"./compact-item-c03ebbb7.js";import"./slide-cc98ec73.js";import"./CheckOutlined-9e94d9a6.js";import"./CloseCircleFilled-8383c3aa.js";import"./CloseOutlined-d6b1b11b.js";import"./DownOutlined-67df1f86.js";import"./SearchOutlined-84e5fa13.js";import"./SelectionValue-320cacef.js";import"./LocaleContext-5ea78dca.js";const N={title:"Work in progress/Widgets/Boolean/BooleanSearch",component:n,argTypes:{...c}},v=o=>{const[u,r]=p.useState(o.value);return p.useEffect(()=>{r(o.value)},[o.value]),l.jsx(n,{...o,value:u,onChange:t=>{var a;r(t),(a=o.onChange)==null||a.call(o,t)}})},e=v.bind({});e.args={value:"true"};var m,i,s;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <BooleanSearch {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(s=(i=e.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const P=["Basic"];export{e as Basic,P as __namedExportsOrder,N as default};
---
+++ develop/assets/Many2OneDummy.stories-db96c284.js
@@ -0,0 +1,82 @@
+import{j as H}from"./jsx-runtime-f6d73c06.js";import{r as v}from"./index-8ee6c85d.js";import{M as z}from"./Many2OneDummy-e3558c6a.js";import{B as I}from"./BaseField.argTypes-d72d00ba.js";import"./_commonjsHelpers-de833af9.js";import"./styled-components.browser.esm-f71162e2.js";import"./index-f36c3419.js";import"./assertThisInitialized-4fab7dd0.js";import"./index-8eb238e3.js";import"./Field-074a9b5f.js";import"./Label-517a773f.js";import"./index-d8bbe3ff.js";import"./toArray-d3285e7a.js";import"./isVisible-28761958.js";import"./index-f3897118.js";import"./motion-343f592f.js";import"./roundedArrow-eeec1c6c.js";import"./reactNode-c432c2b0.js";import"./Compact-f42963ce.js";import"./LocaleContext-5ea78dca.js";import"./context-1cb86123.js";import"./asyncToGenerator-70b03fb5.js";import"./omit-c2a66ff4.js";import"./collapse-97de76d4.js";import"./row-a9151da8.js";import"./useLocale-cc382a13.js";import"./CheckCircleFilled-a3a9ebd2.js";import"./CloseCircleFilled-8383c3aa.js";import"./ExclamationCircleFilled-72a8eb89.js";import"./compact-item-c03ebbb7.js";import"./index-7f6f4650.js";import"./index-8a2d9a84.js";import"./useVariants-98bbef89.js";import"./SearchOutlined-84e5fa13.js";import"./button-5dad7698.js";import"./index-2c809c7b.js";import"./Button-bb28d1e9.js";import"./ButtonWithTooltip-d2c6a913.js";import"./FormigaConfig-2f30d381.js";import"./FolderOpenOutlined-33e43c68.js";const We={title:"Work in progress/Widgets/Many2One/Many2OneDummy",component:z,argTypes:{...I}},e=a=>{const[G,g]=v.useState(a.value);return v.useEffect(()=>{g(a.value)},[a.value]),H.jsx(z,{...a,value:G,onChange:m=>{var d;g(m),(d=a.onChange)==null||d.call(a,m)}})},r=e.bind({});r.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1};const s=e.bind({});s.args={readOnly:!1,required:!0,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1};const t=e.bind({});t.args={readOnly:!0,required:!1,showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const n=e.bind({});n.args={readOnly:!1,required:!1,showFolderButton:!0,showSearchButton:!0,searching:!0,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const u=e.bind({});u.args={readOnly:!1,required:!0,showFolderButton:!0,showSearchButton:!0,searching:!0,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const o=e.bind({});o.args={readOnly:!1,required:!0,showFolderButton:!1,showSearchButton:!1,searching:!1,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const l=e.bind({});l.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};const i=e.bind({});i.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};const c=e.bind({});c.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};var p,h,f;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(f=(h=r.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var S,B,y;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(y=(B=s.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};var b,V,O;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(O=(V=t.parameters)==null?void 0:V.docs)==null?void 0:O.source}}};var C,F,w;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(w=(F=n.parameters)==null?void 0:F.docs)==null?void 0:w.source}}};var D,M,q;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(q=(M=u.parameters)==null?void 0:M.docs)==null?void 0:q.source}}};var E,x,W;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(W=(x=o.parameters)==null?void 0:x.docs)==null?void 0:W.source}}};var _,R,U;l.parameters={...l.parameters,docs:{...(_=l.parameters)==null?void 0:_.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(U=(R=l.parameters)==null?void 0:R.docs)==null?void 0:U.source}}};var j,T,L;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(L=(T=i.parameters)==null?void 0:T.docs)==null?void 0:L.source}}};var N,k,A;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(A=(k=c.parameters)==null?void 0:k.docs)==null?void 0:A.source}}};const _e=["Basic","Required","ReadOnly","Searching","SearchingRequired","NoButtons","ButtonsDisabled","WithSuffixLoading","WithSuffixWithOptions"];export{r as Basic,l as ButtonsDisabled,o as NoButtons,t as ReadOnly,s as Required,n as Searching,u as SearchingRequired,i as WithSuffixLoading,c as WithSuffixWithOptions,_e as __namedExportsOrder,We as default};
