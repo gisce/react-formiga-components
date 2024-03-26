3639cf4ece55c20ee830ebe74b6ae6cdd0d3c2d4
mguellsegarra <mguellsegarra@users.noreply.github.com>
2024-03-26 14:13:29 +0000

    deploy: 1738f0ce518ffc3ac915ca49e248e44dc48e3ac4

--- main/components/widgets/HTMLPreview/HTMLPreview.d.ts
+++ main/components/widgets/HTMLPreview/HTMLPreview.d.ts
@@ -1,4 +1,4 @@
 export declare const HTMLPreview: ({ value }: {
-    value: string;
+    value?: string | undefined;
 }) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=HTMLPreview.d.ts.map
