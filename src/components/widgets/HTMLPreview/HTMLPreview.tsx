20bdc2c87d07fc90702bd1baed339ca49fcb844b
Marc Güell Segarra <marc@ondori.dev>
2024-03-26 15:11:34 +0100

    fix: adjust html preview optional prop

--- src/components/widgets/HTMLPreview/HTMLPreview.tsx
+++ src/components/widgets/HTMLPreview/HTMLPreview.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Markup } from "interweave";
 
-export const HTMLPreview = ({ value }: { value: string }) => {
+export const HTMLPreview = ({ value }: { value?: string }) => {
   return <Markup content={value} allowAttributes />;
 };
