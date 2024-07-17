4f87d672e80e3bfa3baba26d648806d0c064af92
mguellsegarra <mguellsegarra@users.noreply.github.com>
2024-07-17 11:10:34 +0000

    deploy: 6ce5c218aee161fb22333804a07a853e7b93c83d

---
+++ alpha/components/widgets/JSONCheckbox/JSONCheckbox.d.ts
@@ -0,0 +1,3 @@
+import { BaseFieldProps } from '../../../components/form';
+export declare const JSONCheckbox: (props: BaseFieldProps<Record<string, boolean>>) => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=JSONCheckbox.d.ts.map
---
+++ alpha/components/widgets/JSONCheckbox/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./JSONCheckbox";
+//# sourceMappingURL=index.d.ts.map
