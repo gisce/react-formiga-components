7925c2c6ca3e715b26e0b79ad630aa3d220f49d1
mguellsegarra <mguellsegarra@users.noreply.github.com>
2025-04-14 09:23:14 +0000

    deploy: 52b658c003ec239daff4f2ff5e1c01005a4179f1

--- main/components/ui/index.d.ts
+++ main/components/ui/index.d.ts
@@ -7,6 +7,4 @@ export * from "./FavouriteButton";
 export * from "./PaginationHeader";
 export * from "./SelectAllRecordsRow";
 export * from "./SkeletonPill";
-export * from "./Icon";
-export * from "./Notification";
 //# sourceMappingURL=index.d.ts.map
