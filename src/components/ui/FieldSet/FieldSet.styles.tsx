3c2b3d7cad52b7ab9273d64f0b5f23182f7dd1a4
Marc Güell Segarra <marc@ondori.dev>
2025-05-16 09:26:37 +0200

    feat: add colors to fieldset (#50)
    * feat: add colors to fieldset
    
    https://github.com/gisce/webclient/issues/1985
    
    * feat: adjust legend styling

--- src/components/ui/FieldSet/FieldSet.styles.tsx
+++ src/components/ui/FieldSet/FieldSet.styles.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
-export const FieldSetElement = styled.fieldset<{ $borderRadius?: string }>`
+export const FieldSetElement = styled.fieldset<{
+  $borderRadius?: string;
+  $backgroundColor?: string;
+  $borderColor?: string;
+}>`
   padding: 0.5rem;
-  border-color: #d9d9d9;
+  border-color: ${(props) => props.$borderColor || "#d9d9d9"};
   border-width: 1px;
   border-style: solid;
   border-radius: ${(props) => props.$borderRadius || "5px"};
+  background-color: ${(props) => props.$backgroundColor || "transparent"};
   font-family:
     ui-sans-serif,
     system-ui,
@@ -25,8 +30,8 @@ export const FieldSetElement = styled.fieldset<{ $borderRadius?: string }>`
 
 export const Legend = styled.legend`
   all: initial;
-  padding-left: 0.5em;
-  padding-right: 0.5em;
+  padding: 0.25rem 0.75rem;
+  margin: -0.5rem 0.75rem 0;
   font-family:
     system-ui,
     -apple-system,
@@ -38,4 +43,6 @@ export const Legend = styled.legend`
     "Apple Color Emoji",
     "Segoe UI Emoji";
   font-size: 0.85rem;
+  background-color: white;
+  border-radius: 4px;
 `;
