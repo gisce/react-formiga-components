d56f9d7a22b6889acf2f372533ab4e75d983f1b9
ecarreras <ecarreras@users.noreply.github.com>
2025-08-06 09:26:18 +0000

    deploy: 25d90c09938f2b916afd783167a6d8bb5219ae6a

---
+++ alpha/assets/Many2OneDummy.stories-92cd0167.js
@@ -0,0 +1,82 @@
+import{j as H}from"./jsx-runtime-ffb262ed.js";import{r as p}from"./index-76fb7be0.js";import{M as z}from"./Many2OneDummy-4cf24379.js";import{B as I}from"./BaseField.argTypes-d72d00ba.js";import"./_commonjsHelpers-de833af9.js";import"./styled-components.browser.esm-06c7d463.js";import"./index-86654eeb.js";import"./genStyleUtils-28a6e765.js";import"./assertThisInitialized-11245cea.js";import"./Field-346eaafc.js";import"./Label-6c2684e5.js";import"./QuestionCircleOutlined-6633361d.js";import"./QuestionCircleOutlined-04e9cd25.js";import"./AntdIcon-16889e64.js";import"./index-2a201fcf.js";import"./index-fe67bf5a.js";import"./index-356ca88b.js";import"./index-da03a860.js";import"./context-f193351c.js";import"./wrapNativeSuper-e3a961e5.js";import"./Compact-a3364df5.js";import"./useSize-06920fd3.js";import"./reactNode-9d24d027.js";import"./LocaleContext-019b1374.js";import"./index-acf736be.js";import"./useCSSVarCls-56ee7f0e.js";import"./collapse-97de76d4.js";import"./row-8e2bb960.js";import"./AntdIcon-2900fcfe.js";import"./useLocale-8b2da478.js";import"./CheckCircleFilled-e37ba4c2.js";import"./CheckCircleFilled-1392c7ce.js";import"./CloseCircleFilled-1312d6f5.js";import"./CloseCircleFilled-257b815e.js";import"./ExclamationCircleFilled-e4581600.js";import"./ExclamationCircleFilled-eba6e68e.js";import"./compact-item-3b95daf6.js";import"./LoadingOutlined-722c611a.js";import"./index-2bdda7d5.js";import"./index-e1a7f8dc.js";import"./TextArea-80c99f7a.js";import"./useVariants-a27d5c24.js";import"./SearchOutlined-7d2c8869.js";import"./SearchOutlined-11b57711.js";import"./EyeInvisibleOutlined-4021859d.js";import"./button-b6c4a6d6.js";import"./index-64a5fe42.js";import"./Button-b885b3b8.js";import"./ButtonWithTooltip-89661251.js";import"./LoadingOutlined-50148ceb.js";import"./FormigaConfig-c9f78073.js";import"./SearchOutlined-9f8470fd.js";import"./FolderOpenOutlined-fe949aba.js";const Ie={title:"Work in progress/Widgets/Many2One/Many2OneDummy",component:z,argTypes:{...I}},e=a=>{const[G,m]=p.useState(a.value);return p.useEffect(()=>{m(a.value)},[a.value]),H.jsx(z,{...a,value:G,onChange:g=>{var d;m(g),(d=a.onChange)==null||d.call(a,g)}})},r=e.bind({});r.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1};const s=e.bind({});s.args={readOnly:!1,required:!0,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1};const t=e.bind({});t.args={readOnly:!0,required:!1,showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const n=e.bind({});n.args={readOnly:!1,required:!1,showFolderButton:!0,showSearchButton:!0,searching:!0,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const u=e.bind({});u.args={readOnly:!1,required:!0,showFolderButton:!0,showSearchButton:!0,searching:!0,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const o=e.bind({});o.args={readOnly:!1,required:!0,showFolderButton:!1,showSearchButton:!1,searching:!1,disableFolderButton:!1,disableSearchButton:!1,value:"Customer # 1"};const l=e.bind({});l.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};const i=e.bind({});i.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};const c=e.bind({});c.args={readOnly:!1,required:!1,locale:"en_US",showFolderButton:!0,showSearchButton:!0,searching:!1,disableFolderButton:!0,disableSearchButton:!0};var v,h,f;r.parameters={...r.parameters,docs:{...(v=r.parameters)==null?void 0:v.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(f=(h=r.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var S,B,y;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(y=(B=s.parameters)==null?void 0:B.docs)==null?void 0:y.source}}};var b,V,O;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(O=(V=t.parameters)==null?void 0:V.docs)==null?void 0:O.source}}};var C,F,w;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(w=(F=n.parameters)==null?void 0:F.docs)==null?void 0:w.source}}};var D,M,q;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(q=(M=u.parameters)==null?void 0:M.docs)==null?void 0:q.source}}};var E,x,W;o.parameters={...o.parameters,docs:{...(E=o.parameters)==null?void 0:E.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(W=(x=o.parameters)==null?void 0:x.docs)==null?void 0:W.source}}};var _,R,U;l.parameters={...l.parameters,docs:{...(_=l.parameters)==null?void 0:_.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(U=(R=l.parameters)==null?void 0:R.docs)==null?void 0:U.source}}};var j,T,L;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(L=(T=i.parameters)==null?void 0:T.docs)==null?void 0:L.source}}};var N,k,A;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`args => {
+  const [value, setValue] = useState(args.value);
+  useEffect(() => {
+    setValue(args.value);
+  }, [args.value]);
+  return <Many2OneDummy {...args} value={value} onChange={(value: string) => {
+    setValue(value);
+    args.onChange?.(value);
+  }} />;
+}`,...(A=(k=c.parameters)==null?void 0:k.docs)==null?void 0:A.source}}};const Je=["Basic","Required","ReadOnly","Searching","SearchingRequired","NoButtons","ButtonsDisabled","WithSuffixLoading","WithSuffixWithOptions"];export{r as Basic,l as ButtonsDisabled,o as NoButtons,t as ReadOnly,s as Required,n as Searching,u as SearchingRequired,i as WithSuffixLoading,c as WithSuffixWithOptions,Je as __namedExportsOrder,Ie as default};
