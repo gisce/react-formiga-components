e289406ef07418d08ab2aa12676056264a8a3ddb
Marc Güell Segarra <marc@ondori.dev>
2025-08-19 21:10:23 +0200

    feat: new toggle dropdown button for saved searches use
    https://github.com/gisce/webclient/issues/2413

--- src/components/ui/FavouriteButton/FavouriteButton.tsx
+++ src/components/ui/FavouriteButton/FavouriteButton.tsx
@@ -1,11 +1,10 @@
-import { StarOutlined, StarFilled, DownOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { StarOutlined, StarFilled } from "@ant-design/icons";
 import {
   FavouriteButtonProps,
   FavouriteButtonRef,
 } from "./FavouriteButton.types";
-import { Fragment, forwardRef, memo, useImperativeHandle, useRef } from "react";
-import { Dropdown } from "@/components";
+import { forwardRef, memo } from "react";
+import { ToggleDropdownButton } from "../ToggleDropdownButton/ToggleDropdownButton";
 
 export const FavouriteButtonComponent = forwardRef<
   FavouriteButtonRef,
@@ -13,50 +12,15 @@ export const FavouriteButtonComponent = forwardRef<
 >((props: FavouriteButtonProps, ref) => {
   const { isFavourite, onToggleFavourite, ...rest } = props;
 
-  const dropdownRef = useRef<FavouriteButtonRef>(null);
-
-  useImperativeHandle(ref, () => ({
-    close: () => dropdownRef.current?.close(),
-  }));
-
   return (
-    <Fragment>
-      <Button
-        type={isFavourite ? "primary" : "default"}
-        style={{
-          width: 50,
-          borderTopRightRadius: 0,
-          borderBottomRightRadius: 0,
-          padding: 0,
-        }}
-        onClick={onToggleFavourite}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {isFavourite ? (
-            <StarFilled style={{ fontSize: "150%", color: "white" }} />
-          ) : (
-            <StarOutlined style={{ fontSize: "150%" }} />
-          )}
-        </div>
-      </Button>
-      <Dropdown ref={dropdownRef} {...rest}>
-        <Button
-          style={{
-            width: 25,
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-          }}
-          icon={<DownOutlined style={{ fontSize: "0.5em" }} />}
-          onClick={(e) => e.preventDefault()}
-        ></Button>
-      </Dropdown>
-    </Fragment>
+    <ToggleDropdownButton
+      ref={ref}
+      isActive={isFavourite}
+      onToggle={onToggleFavourite}
+      activeIcon={<StarFilled style={{ fontSize: "150%", color: "white" }} />}
+      inactiveIcon={<StarOutlined style={{ fontSize: "150%" }} />}
+      {...rest}
+    />
   );
 });
 
