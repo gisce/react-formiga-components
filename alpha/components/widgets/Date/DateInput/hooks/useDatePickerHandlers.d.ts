38f363c35f5e9c7b8f99eac666b2fa752d29d4b7
mguellsegarra <mguellsegarra@users.noreply.github.com>
2025-12-24 09:41:12 +0000

    deploy: 109da749df119d69ce3c4600c5cc4b52250625ee

--- alpha/components/widgets/Date/DateInput/hooks/useDatePickerHandlers.d.ts
+++ alpha/components/widgets/Date/DateInput/hooks/useDatePickerHandlers.d.ts
@@ -7,7 +7,6 @@ type UseDatePickerHandlersParams = {
 export declare const useDatePickerHandlers: ({ mode, showTime, onChange, }: UseDatePickerHandlersParams) => {
     handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
     handleBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
-    handleDoubleClick: (e: React.MouseEvent<HTMLInputElement>) => void;
 };
 export {};
 //# sourceMappingURL=useDatePickerHandlers.d.ts.map
